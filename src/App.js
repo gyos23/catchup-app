f63bb0830fb22904707b2e291453089d8feaebab
Dorian L. <gyos23@users.noreply.github.com>
2025-09-26 23:21:53 +0100

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,1 +1,12 @@
+import React from 'react';
+import CatchUpDashboard from './components/CatchUpDashboard';
 
+function App() {
+  return (
+    <div className="App">
+      <CatchUpDashboard />
+    </div>
+  );
+}
+
+export default App;
